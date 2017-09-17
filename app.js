9998ec5025076b7014f91c252a924d5f2ff3c4ad
Michelle Torres <michelle.torres@corb.mx>
2017-09-17 13:52:58 -0500

    Adding message process and responses

--- app.js
+++ app.js
@@ -61,11 +61,112 @@ app.post('/webhook', function (req, res) {
   console.log("message received!");
   var data = req.body;
   if (data.object == 'page') {
-    let message = data.entry[0].messaging[0].message;
-    console.log(message.text);
+    // entries from multiple pages may be batched in one request
+    data.entry.forEach(function(pageEntry) {
+      let message = pageEntry.messaging[0].message;
+      console.log(message.text);
+
+      // iterate over each messaging event for this page
+      pageEntry.messaging.forEach(function(messagingEvent) {
+
+        if (messagingEvent.message) {
+          processMessageFromPage(messagingEvent);
+        } else {
+          console.log("[app.post] not prepared to handle this message type.");
+        }
+      });
+    });
     res.sendStatus(200);
   }
 });
+
+/*
+* called when a message is sent to your page
+*
+*/
+function processMessageFromPage(event) {
+  let senderID = event.sender.id;
+  let pageID = event.recipient.id;
+  let timeOfMessage = event.timestamp;
+  let message = event.message;
+  console.log("[processMessageFromPage] user (%d) page (%d) timestamp (%d) and message (%s)",
+              senderID, pageID, timeOfMessage, JSON.stringify(message));
+  // the 'message' object format varies depending on the message type
+  let messageText = message.text;
+  if (messageText) {
+    console.log("[processMessageFromPage]: %s", messageText);
+    sendTextMessage(senderID, messageText);
+  }
+}
+
+/*
+ * Verify that the request came from Facebook. You should expect a hash of
+ * the App Secret from your App Dashboard to be present in the x-hub-signature
+ * header field.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // In DEV, log an error. In PROD, throw an error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    console.log("received  %s", signatureHash);
+    console.log("exepected %s", expectedHash);
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
+/*
+* Send a text message using the Send API.
+* @recipientId integer senderID
+*/
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText // utf-8, 640-character max
+    }
+  };
+  console.log("[sendTextMessage] %s", JSON.stringify(messageData));
+  callSendAPI(messageData);
+}
+
+/*
+* Call the Send API. If the call succeeds, the
+* message id is returned in the response.
+*
+*/
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log("[callSendAPI] success!");
+      } else {
+        console.error("[callSendAPI] Send API call failed");
+      }
+  });
+}
 
 /*
  * Start your server
